18ad78ec1153aac451e48a146941cf1e58c49e31
Jorge Martin <jorgeestudios.martin@gmail.com>
2025-04-02 13:37:39 +0200

    added error to useUser arrow function

--- harmonia/hooks/useUser.tsx
+++ harmonia/hooks/useUser.tsx
@@ -2,7 +2,7 @@ import { User } from "@supabase/auth-helpers-nextjs";
 import { useSessionContext,
          useUser as useSupaUser }
 from "@supabase/auth-helpers-react";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 import { Subscription, UserDetails } from "@/types";
 
@@ -79,4 +79,12 @@ export const MyUserContextProvider = (props: Props) => {
     };
 
     return <UserContext.Provider value={value} {...props} />
+};
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if(context === undefined) {
+        throw new Error('useUser must be used within its context (MyUserContextProvider)');
+    }
+    return context;
 }
