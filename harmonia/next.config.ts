67e92d3b1c372ce694675116429b67f1eaca2ca9
Coral <coralcamachodapena@gmail.com>
2025-04-16 17:13:41 +0200

    Corrección next.config
    he añadido nuestra Reference ID para que cuando se suban las canciones y la imagen que acompaña cada canción esta última no de conflicto

--- harmonia/next.config.ts
+++ harmonia/next.config.ts
@@ -1,12 +1,14 @@
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     domains: [
-      /*.supabase.co"   // FALTA POR AÑADIR, HORA DEL VIDEO 3HORAS Y 10 MIN*/ 
+      "midxvehuoobspqzawtpw.supabase.co" // FALTA POR AÑADIR, HORA DEL VIDEO 3HORAS Y 10 MIN*/ 
     ]
    } /* config options here */
 };
-
+module.exports = nextConfig;
 export default nextConfig;
 
