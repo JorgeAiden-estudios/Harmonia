b0315a4a1f584135909502da11e7939843d7d988
Coral <coralcamachodapena@gmail.com>
2025-04-16 17:54:01 +0200

    corrección searchInput
    corrección searchInput

--- harmonia/components/SearchInput.tsx
+++ harmonia/components/SearchInput.tsx
@@ -4,7 +4,7 @@ import qs from "query-string";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-import { useDebounce } from "@/hooks/useDebounce";
+import  useDebounce  from "@/hooks/useDebounce";
 
 import Input from "./Input";
 
